850add3b86c709dfd4979121508f0a030c0d824d
arjit321 <arjit_sharma@canalys.com>
2024-10-10 10:08:25 +0530

    Handling Parenthesis - example code

--- examples/test.hs
+++ examples/test.hs
@@ -1,12 +1,11 @@
-PRINT "Enter a"
-INPUT a
-PRINT "Enter b"
-INPUT b
+LET x =  ((2 + 3) * 5 )
+PRINT x
 
-IF a>b THEN
-    PRINT "a is greater"
+
+IF ((x+1 - ( (20 + 3 ) * 4)) > 20 ) THEN
+    PRINT "aaaaaaaaaaaaaa"
 ENDIF
 
-IF b>a THEN 
-    PRINT "b is greater"
-ENDIF
+WHILE ((x+1 - ( (20 + 3 ) * 4)) > 20 ) REPEAT
+    PRINT "aaa"
+ENDWHILE
